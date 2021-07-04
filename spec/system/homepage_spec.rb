12dbf80f590cff456f302bcabd534392d4f7ca1d
Peichong <fupeichong@gmail.com>
2021-07-04 17:53:27 +0800

    update placeholder homepage spec to utilise javascript

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Homepage", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  before { visit '/' }
 
-  describe "page is displayed correctly" do
-    it "is a test" do
+  describe "page is displayed correctly", js: true do
+    it "has a h1 heading" do
+      h1_text = 'Welcome to the Stock Market Prediction App'
+      expect(page).to have_css('h1', text: h1_text)
+    end
+
+    it "has a h2 heading" do
+      h2_text = 'CSIT321 Final Year Project'
+      expect(page).to have_css('h2', text: h2_text)
+    end
+
+    it "has all group member names" do
+      member_selector = '.members-container > span'
+      members = [ 'Jonathan', 'Ling Yi', 'PC', 'Joey', 'Muru' ]
+      members.each {|m| expect(page).to have_css(member_selector, text: m)}
     end
   end
 end
