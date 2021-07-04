697fdf2c52c3608b74b0bfad468c40c9bced923c
Peichong <fupeichong@gmail.com>
2021-07-04 17:22:55 +0800

    add mock system test for placeholder homepage

---
+++ spec/system/homepage_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Homepage", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe "page is displayed correctly" do
+    it "is a test" do
+    end
+  end
+end
