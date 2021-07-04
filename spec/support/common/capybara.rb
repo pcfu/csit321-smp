6edd688238f975a57d5d7a6e2394dd6b3bc4adb8
Peichong <fupeichong@gmail.com>
2021-07-04 22:44:49 +0800

    configure capybara for running browser tests in WSL

--- spec/support/common/capybara.rb
+++ spec/support/common/capybara.rb
@@ -1,7 +1,7 @@
 Capybara.register_driver :remote_chrome do |app|
   Capybara::Selenium::Driver.new(app,
     browser: :remote,
-    url: 'http://172.17.0.1:9515',
+    url: "http://#{ENV['SELENIUM_HOST']}:9515",
     desired_capabilities: :chrome
   )
 end
@@ -26,11 +26,19 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
-    driven_by headless ? :remote_chrome_headless : :remote_chrome
-
-    Capybara.server_host = `/sbin/ip route|awk '/scope/ { print $9 }'`.strip
-    Capybara.server_port = ENV.fetch('TEST_PORT', 3001)
-    session_server       = Capybara.current_session.server
-    Capybara.app_host    = "http://#{session_server.host}:#{session_server.port}"
+    if headless
+      driven_by :remote_chrome_headless
+      Capybara.server_host = `/sbin/ip route|awk '/scope/ { print $9 }'`.strip
+      Capybara.server_port = ENV.fetch('TEST_PORT', 3001)
+      session_server       = Capybara.current_session.server
+      Capybara.app_host    = "http://#{session_server.host}:#{session_server.port}"
+    else
+      driven_by :remote_chrome
+      Capybara.server      = :puma
+      Capybara.server_host = '0.0.0.0'
+      Capybara.server_port = ENV.fetch('TEST_PORT', 3001)
+      session_server       = Capybara.current_session.server
+      Capybara.app_host    = "http://localhost:#{session_server.port}"
+    end
   end
 end
