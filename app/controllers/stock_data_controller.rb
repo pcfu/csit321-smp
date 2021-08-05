f9a46a2415a2be846705e525e643184dc44ddbdd
Lynn Ng <linglingling000@gmail.com>
2021-08-05 23:57:34 +0800

    amendments to price history and stock index

---
+++ app/controllers/stock_data_controller.rb
@@ -0,0 +1,19 @@
+class StockDataController < ApplicationController
+  has_many :price_histories
+
+  def index
+    shortlist = %w[AAPL GOOG FB TWTR WMT]
+    @shortlist = Stock.where(:symbol => shortlist)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: Stock.all }
+
+    @stocks = Stock.all
+    render layout: 'public_layout'
+  end
+
+  def show
+    @stock = Stock.find(params[:id])
+  end
+end
---
+++ app/javascript/packs/stocks/index.js
@@ -0,0 +1,16 @@
+$(document).on('turbolinks:load', function () {
+    $.get('stocks.json', function (data) {
+      window.$('#stocks-list').DataTable({
+        aaData: data,
+        columns: [
+          { data: 'id' },
+          { data: 'symbol' },
+          { data: 'name' },
+          { data: 'exchange' },
+          { data: 'stock_type' },
+          { data: 'description' },
+        ]
+      });
+    });
+  });
+  
