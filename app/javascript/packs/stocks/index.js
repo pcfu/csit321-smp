a1e1e8d581d373c9a30779de2ad4d6ca752196a0
Peichong <fupeichong@gmail.com>
2021-08-07 03:05:22 +0800

    partial clean up of stocks index page

--- app/javascript/packs/stocks/index.js
+++ app/javascript/packs/stocks/index.js
@@ -1,16 +1,19 @@
 $(document).on('turbolinks:load', function () {
-    $.get('stocks.json', function (data) {
-      window.$('#stocks-list').DataTable({
-        aaData: data,
-        columns: [
-          { data: 'id' },
-          { data: 'symbol' },
-          { data: 'name' },
-          { data: 'exchange' },
-          { data: 'stock_type' },
-          { data: 'description' },
-        ]
-      });
+  $.get('stocks.json', function (data) {
+    window.$('#stocks-list').DataTable({
+      aaData: data,
+      columns: [
+        { data: 'id' },
+        { data: 'symbol' },
+        { data: 'name' },
+        { data: 'exchange' },
+        { data: 'stock_type' },
+        { data: 'description' },
+      ]
     });
   });
-  
+
+  $(".clickable-row").click(function() {
+    window.location = $(this).data("href");
+  });
+});
