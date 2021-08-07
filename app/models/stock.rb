3a619d0376b0847e14eb89172df39540621a7392
Peichong <fupeichong@gmail.com>
2021-08-07 14:44:26 +0800

    update Stock model validation and methods

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,19 @@
 class Stock < ApplicationRecord
   has_many :price_histories, dependent: :destroy
+
+  auto_strip_attributes :symbol, :name, :exchange, :stock_type, :description
+
+  validates :symbol,      presence: true, uniqueness: true
+  validates :name,        presence: true
+  validates :exchange,    presence: true
+  validates :stock_type,  presence: true
+
+  before_validation :upcase_symbol
+
+
+  private
+
+    def upcase_symbol
+      self.symbol.upcase! if symbol.present?
+    end
 end
