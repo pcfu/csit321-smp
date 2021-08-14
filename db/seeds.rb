f675ecdda402ed9fc658989d5131c0d4006a9eec
Peichong <fupeichong@gmail.com>
2021-08-14 10:33:04 +0800

    add seed data for stock price histories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-filepath = "#{Rails.root}/db/stocks_list.json"
+DATA_DIR = "#{Rails.root}/db/seed_data"
+
+
+puts "=== INSERTING STOCKS ==="
+
+filepath = "#{DATA_DIR}/stocks_list.json"
 stocks = JSON.parse(File.read(filepath))
 stocks.each {|stock| Stock.create(stock)}
 
-#Price History Dummy Data
-filepath = "#{Rails.root}/db/pricehistory_list.json"
-pricehistories = JSON.parse(File.read(filepath))
-pricehistories.each {|pricehistory| PriceHistory.create(pricehistory)}
+
+puts "=== INSERTING PRICE HISTORIES ==="
+
+filepath = "#{DATA_DIR}/price_histories_list.json"
+price_data = JSON.parse(File.read(filepath))
+price_data.each do |symbol, prices|
+  stock = Stock.find_by(:symbol => symbol)
+
+  prices.each_with_index do |prc, idx|
+    print "\rInserting price for #{symbol} (#{idx + 1} of #{prices.count})"
+    stock.price_histories.create prc
+  end
+  puts
+end
