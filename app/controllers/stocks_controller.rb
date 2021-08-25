4032c45758c7195269e18f356b53e2271f210e49
Peichong <fupeichong@gmail.com>
2021-08-26 02:04:52 +0800

    update stocks/show endpoint - controller, erb, and js

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,7 +8,9 @@ class StocksController < ApplicationController
 
   def show
     @stock = Stock.find(params[:id])
+    @recent_predictions = @stock.price_predictions.order(entry_date: :desc).limit(2)
     @prediction_attrs = PricePrediction.const_get(:ATTRS).map &:to_s
+
     respond_to do |format|
       format.html
       #format.json {render :json => Stock.PricePredict.all }
--- app/javascript/packs/stocks/show.js
+++ app/javascript/packs/stocks/show.js
@@ -1,5 +1,134 @@
 import Chart from 'chart.js/auto';
 
+const ALPHA   = '55';
+const GREEN   = '#12C91E';
+const PURPLE  = '#8E5EA2';
+const RED     = '#F26370';
+const DIAMOND = 'rectRot';
+const PT_SIZE = 2;
+const X_AXIS  = {
+  MIN: 1,
+  MAX: 365,
+  MILESTONES: [1, 14, 90, 365]
+};
+
+
+function maxPriceData(pd) {
+  return {
+    data: [
+      { x: pd.nd_day, y: pd.nd_max_price },
+      { x: pd.st_day, y: pd.st_max_price },
+      { x: pd.mt_day, y: pd.mt_max_price },
+      { x: pd.lt_day, y: pd.lt_max_price }
+    ],
+    label: "Maximum",
+    pointStyle: DIAMOND,
+    pointBorderWidth: PT_SIZE,
+    fill: '+1',
+    borderColor: GREEN,
+    backgroundColor: GREEN + ALPHA,
+    pointBackgroundColor: GREEN,
+    order: 3
+  };
+}
+
+function expPriceData(pd) {
+  return {
+    data: [
+      { x: pd.nd_day, y: pd.nd_exp_price },
+      { x: pd.st_day, y: pd.st_exp_price },
+      { x: pd.mt_day, y: pd.mt_exp_price },
+      { x: pd.lt_day, y: pd.lt_exp_price }
+    ],
+    label: "Expected",
+    pointStyle: DIAMOND,
+    pointBorderWidth: PT_SIZE,
+    fill: '+1',
+    borderColor: PURPLE,
+    backgroundColor: RED + ALPHA,
+    pointBackgroundColor: PURPLE,
+    order: 2
+  };
+}
+
+function minPriceData(pd) {
+  return {
+    data: [
+      { x: pd.nd_day, y: pd.nd_min_price },
+      { x: pd.st_day, y: pd.st_min_price },
+      { x: pd.mt_day, y: pd.mt_min_price },
+      { x: pd.lt_day, y: pd.lt_min_price }
+    ],
+    label: "Mininum",
+    pointStyle: DIAMOND,
+    pointBorderWidth: PT_SIZE,
+    borderColor: RED,
+    pointBackgroundColor: RED,
+    order: 1
+  };
+}
+
+function scaleOptions() {
+  return {
+    x: {
+      type: 'linear',
+      display: true,
+      ticks: {
+        min: X_AXIS.MIN,
+        max: X_AXIS.MAX,
+        stepSize: 1,
+        minRotation: 45,
+        callback: function(value, index, values) {
+          const dataPoints = X_AXIS.MILESTONES;
+          const labels = this.chart.data.labels;
+          return labels[dataPoints.indexOf(value)];
+        }
+      }
+    }
+  };
+}
+
+function legendOptions() {
+  return {
+    reverse: true,
+    labels: {
+      font: { size: 20 },
+      padding: 10,
+      generateLabels: function(chart) {
+        const labels = Chart.defaults.plugins.legend.labels.generateLabels(chart);
+        labels.forEach(l => l.fillStyle = l.strokeStyle);
+        return labels;
+      }
+    }
+  };
+}
+
+function tooltipOptions() {
+  return {
+    titleAlign: 'center',
+    bodyAlign: 'center',
+    usePointStyle: true,
+    callbacks: {
+      title: function(context) {
+        const dates = context[0].chart.data.labels;
+        const terms = ['next day', 'short term', 'medium term', 'long term'];
+        const idx = context[0].dataIndex;
+        return `${dates[idx]} (${terms[idx]})`;
+      },
+      label: function(context) {
+        return `${context.dataset.label} Price: ${context.formattedValue}`;
+      },
+      labelPointStyle: function(context) {
+        return { pointStyle: DIAMOND };
+      }
+    }
+  };
+}
+
+
+let chart = null;
+
+
 $(document).on('turbolinks:load', function () {
   // Trading View Chart
   var ex_symbol = $('#stockexchange').html()
@@ -22,63 +151,30 @@ $(document).on('turbolinks:load', function () {
   });
 
 
-  //Get the price predict values
-  var price_prediction = $('.price_prediction').data('pxpredict');
-  $.each(price_prediction, function() {
+  // Prediction Chart
+  const pd = $('.price_prediction').data('pxpredict');
+  const chartData = {
+    labels: [ pd.nd_date, pd.st_date, pd.mt_date, pd.lt_date ],
+    datasets: [ maxPriceData(pd), expPriceData(pd), minPriceData(pd) ]
+  };
 
-    //Prediction Chart
-    new Chart($("#line-chart"), {
-      type: 'line',
-      data: {
-        labels: [
-          this['nd_date'], this['st_date'], this['mt_date'], this['lt_date']
-        ],
-        datasets: [{
-          data: [
-            this['nd_max_price'],
-            this['st_max_price'],
-            this['mt_max_price'],
-            this['lt_max_price']
-          ],
-          label: "Max",
-          borderColor: "#61f29d",
-          backgroundColor: "#61f29d",
-          fill: true,
-          order: 3
-        }, {
-          data: [
-            this['nd_exp_price'],
-            this['st_exp_price'],
-            this['mt_exp_price'],
-            this['lt_exp_price']
-          ],
-          label: "Expected",
-          borderColor: "#8e5ea2",
-          backgroundColor: "#8e5ea2",
-          fill: true,
-          order: 2
-        }, {
-          data: [
-            this['nd_min_price'],
-            this['st_min_price'],
-            this['mt_min_price'],
-            this['lt_min_price']
-          ],
-          label: "Min",
-          borderColor: "#f26370",
-          backgroundColor: "#f26370",
-          fill: true,
-          order: 1
-        },
-        ]
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'Expected Returns Prediction'
-        }
-      }
-    });
+  const options = {
+    scales: scaleOptions(),
+    plugins: {
+      legend: legendOptions(),
+      tooltip: tooltipOptions()
+    }
+  };
+
+  chart = new Chart($("#line-chart"), {
+    type: 'line',
+    data: chartData,
+    options: options
   });
+});
 
+
+$(document).on('turbolinks:before-visit', function (){
+  if (chart) chart.destroy();
+  chart = null;
 });
