ae17950a41c4330991e4bd38dca038807ac7d7d3
Peichong <fupeichong@gmail.com>
2021-08-28 05:52:46 +0800

    update model training ajax call on prototype page

--- app/javascript/packs/admin/prototype/index.js
+++ app/javascript/packs/admin/prototype/index.js
@@ -4,7 +4,12 @@ $(document).on('turbolinks:load', function () {
   $('#training-button').on('click', function () {
     const alertsContainer = $('#alerts-container');
 
-    $.post('/admin/model_trainings', function(data) {
+    const content = JSON.stringify({
+      config_id: 1,
+      data_range: ['2020-01-01', '2021-01-01']
+    });
+
+    $.post('/admin/model_trainings', { json: content }, function(data) {
       Alerts.success(alertsContainer, data.message);
 
     }).fail(function(xhr, status, error) {
