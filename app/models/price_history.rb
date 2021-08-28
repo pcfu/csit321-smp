f5d719dcb3c1cf24285309a63c64a917cdcd70ca
Peichong <fupeichong@gmail.com>
2021-08-29 07:31:41 +0800

    PriceHistory - add scopes to help date searches

--- app/models/price_history.rb
+++ app/models/price_history.rb
@@ -1,6 +1,9 @@
 class PriceHistory < ApplicationRecord
   belongs_to :stock
 
+  scope :start, ->(date) { where('date >= ?', date || '-infinity') }
+  scope :end,   ->(date) { where('date <= ?', date || 'infinity') }
+
   validates_presence_of :date, :open, :high, :low, :close,
                         :volume, :change, :percent_change
   validates_numericality_of :open, :high, :low, :close, :volume,
