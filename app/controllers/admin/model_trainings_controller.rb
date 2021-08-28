58c42479fbf96f79a7b028a76a30c08a85f48724
Peichong <fupeichong@gmail.com>
2021-08-29 03:25:33 +0800

    cleanup pipeline to request and enqueue model training jobs

--- app/controllers/admin/model_trainings_controller.rb
+++ app/controllers/admin/model_trainings_controller.rb
@@ -4,11 +4,13 @@ module Admin
 
     def create
       with_error_handling do |flagger|
-        res = BackendClient.post('/ml/model_training', create_params)
-        res_body = JSON.parse(res.body).symbolize_keys
-        flagger.flag(res_body) if res_body[:status] != 'ok'
+        params = create_params
+        @model_config = ModelConfig.find_by(id: params[:config_id])
 
-        render json: JSON.parse(res.body)
+        reset_trainings(@model_config, params)
+        response = request_backend_to_enqueue_jobs(params)
+        update_trainings(@model_config, response[:results])
+        render json: response
       end
     end
 
@@ -16,7 +18,37 @@ module Admin
     private
 
       def create_params
-        JSON.parse(params.require(:json)).merge(stocks: Stock.pluck(:id))
+        # JSON.parse(params.require(:json)).symbolize_keys.merge(stocks: Stock.pluck(:id))
+
+        # for the prototype, just train on AAPL stock
+        JSON.parse(params.require(:json)).symbolize_keys.merge(stocks: [1])
+      end
+
+      def request_backend_to_enqueue_jobs(params)
+        res = BackendClient.post('/ml/model_training', params)
+        res_body = JSON.parse(res.body).symbolize_keys
+        flagger.flag(res_body) if res_body[:status] != 'ok'
+        res_body
+      end
+
+      def reset_trainings(model_config, params)
+        date_start = params[:data_range][0]
+        date_end = params[:data_range][1]
+        stock_ids = params[:stocks]
+        model_config.reset_trainings(date_start, date_end, stock_ids)
+      end
+
+      def update_trainings(model_config, results)
+        results.each do |result|
+          result.symbolize_keys!
+          trng = model_config.model_trainings.find_by(stock_id: result[:stock_id])
+
+          if result[:status] == 'ok'
+            trng.update(stage: :enqueued)
+          else
+            trng.update(stage: :error, error_message: result[:error_message])
+          end
+        end
       end
   end
 end
