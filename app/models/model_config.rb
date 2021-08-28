d369b3b78ee5261dc0d21b6db4e549a0cb7b36d7
Peichong <fupeichong@gmail.com>
2021-08-29 02:56:09 +0800

    ModelConfig - add method to reset associated model trainings

--- app/models/model_config.rb
+++ app/models/model_config.rb
@@ -14,9 +14,24 @@ class ModelConfig < ApplicationRecord
                                             greater_than_or_equal_to: 0,
                                             less_than_or_equal_to: 100 }
 
+
   def set_train_percent
     num_done = model_trainings.done.count
     update(train_percent: (num_done.to_f / Stock.count * 100).to_i)
     return self
   end
+
+  def reset_trainings(date_start, date_end, stock_ids = nil)
+    self.train_percent = 0.0
+
+    stock_ids ||= Stock.pluck(:id)
+    stock_ids.each do |sid|
+      trng = ModelTraining.find_or_initialize_by(model_config_id: id, stock_id: sid)
+      trng.reset.assign_attributes(date_start: date_start, date_end: date_end)
+      trng.save
+    end
+
+    save
+  end
+
 end
--- spec/models/model_config_spec.rb
+++ spec/models/model_config_spec.rb
@@ -77,7 +77,6 @@ RSpec.describe ModelConfig, type: :model do
       let(:num_stocks)    { 10 }
       let(:num_trainings) { 9 }
       let(:num_done)      { rand(3..7) }
-      subject(:config)    { create :model_config }
 
       it "sets train_percent to ratio of done model_trainings / total stocks" do
         num_stocks.times do |i|
@@ -85,14 +84,80 @@ RSpec.describe ModelConfig, type: :model do
           next if i >= num_trainings
 
           if i < num_done
-            create(:model_training, :done, model_config_id: config.id, stock_id: stock.id)
+            create(:model_training, :done, model_config_id: ctrl_config.id, stock_id: stock.id)
           else
-            create(:model_training, model_config_id: config.id, stock_id: stock.id)
+            create(:model_training, model_config_id: ctrl_config.id, stock_id: stock.id)
           end
         end
 
         expected = (num_done.to_f / num_stocks * 100).to_i
-        expect(config.set_train_percent.train_percent).to eq(expected)
+        expect(ctrl_config.set_train_percent.train_percent).to eq(expected)
+      end
+    end
+
+    describe "reset_trainings" do
+      before do
+        10.times { create :boilerplate_stock }
+      end
+
+      let(:stock)  { Stock.first }
+      let(:date_s) { Date.new(2020, 1, 1) }
+      let(:date_e) { Date.new(2021, 1, 1) }
+
+      it "sets train percent to 0" do
+        ctrl_config.reset_trainings(date_s, date_e)
+        expect(ctrl_config.reload.train_percent).to eq(0.0)
+      end
+
+      it "creates model_trainings if they don't exist" do
+        expect {
+          ctrl_config.reset_trainings(date_s, date_e, [stock.id])
+        }.to change(ModelTraining, :count).by 1
+      end
+
+      it "does not create model_trainings if they exist" do
+        trng = create(:full_training, model_config_id: ctrl_config.id, stock_id: stock.id)
+        expect {
+          ctrl_config.reset_trainings(date_s, date_e, [stock.id])
+        }.to change(ModelTraining, :count).by 0
+      end
+
+      it "resets model_trainings to default states only for specified stocks" do
+        Stock.pluck(:id).each do |sid|
+          create(:full_training, model_config_id: ctrl_config.id, stock_id: sid)
+        end
+
+        reset_ids = Stock.limit(5).pluck(:id)
+        ctrl_config.reset_trainings(date_s, date_e, reset_ids)
+
+        trng_attrs = attributes_for(:full_training)
+        ModelTraining.all.each do |trng|
+          if trng.stock_id.in? reset_ids
+            expect(trng.requested?).to be true
+            expect(trng.rmse.nil?).to be true
+            expect(trng.error_message.nil?).to be true
+          else
+            expect(trng.stage.to_s).to eq(trng_attrs[:stage].to_s)
+            expect(trng.rmse).to eq(trng_attrs[:rmse])
+            expect(trng.error_message).to eq(trng_attrs[:error_message])
+          end
+        end
+      end
+
+      context "when stocks ids not specified" do
+        it "reset model_trainings for all stocks" do
+          Stock.pluck(:id).each do |sid|
+            create(:full_training, model_config_id: ctrl_config.id, stock_id: sid)
+          end
+
+          ctrl_config.reset_trainings(date_s, date_e)
+
+          ModelTraining.all.each do |trng|
+            expect(trng.requested?).to be true
+            expect(trng.rmse.nil?).to be true
+            expect(trng.error_message.nil?).to be true
+          end
+        end
       end
     end
   end
