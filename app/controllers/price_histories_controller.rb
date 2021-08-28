df006a46ec3e930955157529425925a493125f35
Peichong <fupeichong@gmail.com>
2021-08-29 07:32:17 +0800

    PriceHistoriesController - add date ranges when searching for prices

--- app/controllers/price_histories_controller.rb
+++ app/controllers/price_histories_controller.rb
@@ -1,6 +1,8 @@
 class PriceHistoriesController < ApplicationController
   def index
     @price_histories = Stock.find(params[:stock_id]).price_histories
+                            .start(params[:date_start]).end(params[:date_end])
+
     respond_to do |format|
       format.json
       format.any { render_404 }
