0cef1be4d1df86df06371ebf8504cbf8aa2b1e5f
Peichong <fupeichong@gmail.com>
2021-08-29 18:10:22 +0800

    update js for prototype page to match new query requirements

--- app/javascript/packs/admin/prototype/index.js
+++ app/javascript/packs/admin/prototype/index.js
@@ -4,16 +4,19 @@ $(document).on('turbolinks:load', function () {
   $('#training-button').on('click', function () {
     const alertsContainer = $('#alerts-container');
 
-    const content = JSON.stringify({
+    const data = {
       config_id: 1,
       data_range: ['2020-01-01', '2021-01-01']
-    });
+    };
 
-    $.post('/admin/model_trainings', { json: content }, function(data) {
+    $.post('/admin/model_trainings.json', data, function(data) {
       Alerts.success(alertsContainer, data.message);
 
     }).fail(function(xhr, status, error) {
-      Alerts.danger(alertsContainer, xhr.responseJSON.message);
+      Alerts.danger(
+        alertsContainer,
+        xhr.responseJSON ? xhr.responseJSON.message : "Unknown error occurred"
+      );
     });
   })
 });
