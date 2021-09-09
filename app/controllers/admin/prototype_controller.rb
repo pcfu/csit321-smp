f7343700545b284e84912ce69c7f77e63144e55b
Peichong <fupeichong@gmail.com>
2021-09-09 18:31:39 +0800

    prototype page - add predictions table

--- app/controllers/admin/prototype_controller.rb
+++ app/controllers/admin/prototype_controller.rb
@@ -2,6 +2,15 @@ module Admin
   class PrototypeController < ApplicationController
     def index
       @config = ModelConfig.find(1)
+      @predictions = PricePrediction.order(entry_date: :desc).limit(5)
     end
+
+
+    private
+
+      def prediction_values
+        %w[ entry_date nd_date nd_max_price nd_exp_price nd_min_price
+            st_date st_max_price st_exp_price st_min_price ]
+      end
   end
 end
