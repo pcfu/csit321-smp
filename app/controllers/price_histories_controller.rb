a60ec35ddc9e448140f9dd57e3791faf408e0ca2
Peichong <fupeichong@gmail.com>
2021-09-10 18:16:14 +0800

    PriceHistoriesController - enable filtering of attributes

--- app/controllers/price_histories_controller.rb
+++ app/controllers/price_histories_controller.rb
@@ -2,6 +2,7 @@ class PriceHistoriesController < ApplicationController
   def index
     @price_histories = Stock.find(params[:stock_id]).price_histories
                             .start(params[:date_start]).end(params[:date_end])
+    @fields = params[:fields]
 
     respond_to do |format|
       format.json
--- app/views/price_histories/index.json.jbuilder
+++ app/views/price_histories/index.json.jbuilder
@@ -4,6 +4,8 @@ json.status 'ok'
 json.price_histories do
   json.array! @price_histories do |p_hist|
     attrs.each do |attr|
+      next if @fields.present? and !@fields.include?(attr)
+
       val = p_hist[attr]
       json.set! attr, val.is_a?(BigDecimal) ? val.to_f : val
     end
