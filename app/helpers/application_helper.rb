2ca07640fe27a48cb4881ca9bb63e9a6ca7abd1d
Peichong <fupeichong@gmail.com>
2021-09-25 19:21:06 +0800

    ApplicationHelper - add tag helper to generate form labels with errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def form_label_with_error(form, field, model)
+    content_tag(:div, class: "form-label-group d-flex justify-content-between") do
+      concat(form.label field)
+      if model.errors[field].present?
+        concat(content_tag(:span, model.errors[field].first))
+      end
+    end
+  end
 end
