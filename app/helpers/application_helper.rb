c583f2206125fd6603f927b02f9a3868f45b1964
Peichong <fupeichong@gmail.com>
2021-09-25 21:58:03 +0800

    improve styling of user registration page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     content_tag(:div, class: "form-label-group d-flex justify-content-between") do
       concat(form.label field)
       if model.errors[field].present?
-        concat(content_tag(:span, model.errors[field].first))
+        concat(content_tag(:span, model.errors[field].first, class: 'field-error-message'))
       end
     end
   end
