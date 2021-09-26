8f59490c869b85e78b9565dcc69d0281145cfb58
Peichong <fupeichong@gmail.com>
2021-09-27 00:11:21 +0800

    update registration request spec

--- spec/requests/registration_spec.rb
+++ spec/requests/registration_spec.rb
@@ -3,24 +3,34 @@ require 'rails_helper'
 RSpec.describe "Registration", type: :request do
   let(:valid_input)   { attributes_for(:user) }
   let(:invalid_input) { attributes_for(:user, :pw_too_short) }
+  let(:credentials)   { valid_input.slice(:email, :password) }
 
   describe "GET /register" do
-    it "returns http success" do
-      get '/register'
-      expect(response).to have_http_status(:success)
+    context "when logged out" do
+      it "returns http success" do
+        get '/register'
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "when logged in" do
+      before do
+        create :user
+        login_user credentials
+      end
+
+      it "returns redirect" do
+        get '/register'
+        expect(response).to be_redirect
+      end
     end
   end
 
   describe "POST /register" do
     context "when valid input" do
-      # it "logs in user and returns redirect" do
-      #   post '/register', params: { user: attributes_for(:user) }
-      #   expect(controller.logged_in?).to be true
-      #   expect(response).to be_redirect
-      # end
-
-      it "returns redirect" do
+      it "logs in user and returns redirect" do
         post '/register', params: { user: valid_input }
+        expect(controller.logged_in?).to be true
         expect(response).to be_redirect
       end
     end
@@ -28,7 +38,7 @@ RSpec.describe "Registration", type: :request do
     context "when invalid input" do
       it "it returns conflict" do
         post '/register', params: { user: invalid_input }
-        #expect(controller.logged_in?).to be false
+        expect(controller.logged_in?).to be false
         expect(response).to have_http_status(:conflict)
       end
     end
