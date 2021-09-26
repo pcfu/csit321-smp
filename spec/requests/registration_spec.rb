c06e8917d1b766344193ae0c772cb931f03d991d
Peichong <fupeichong@gmail.com>
2021-09-26 13:31:31 +0800

    Request spec - change users to registration spec

--- spec/requests/registration_spec.rb
+++ spec/requests/registration_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe "Registration", type: :request do
+  let(:valid_input)   { attributes_for(:user) }
+  let(:invalid_input) { attributes_for(:user, :pw_too_short) }
+
   describe "GET /register" do
     it "returns http success" do
       get '/register'
@@ -9,22 +12,22 @@ RSpec.describe "Users", type: :request do
   end
 
   describe "POST /register" do
-    context "when valid registration" do
+    context "when valid input" do
       # it "logs in user and returns redirect" do
       #   post '/register', params: { user: attributes_for(:user) }
       #   expect(controller.logged_in?).to be true
       #   expect(response).to be_redirect
       # end
 
-      it "returns http success" do
-        post '/register', params: { user: attributes_for(:user) }
+      it "returns redirect" do
+        post '/register', params: { user: valid_input }
         expect(response).to be_redirect
       end
     end
 
-    context "when invalid registration" do
+    context "when invalid input" do
       it "it returns conflict" do
-        post '/register', params: { user: attributes_for(:user, :pw_too_short) }
+        post '/register', params: { user: invalid_input }
         #expect(controller.logged_in?).to be false
         expect(response).to have_http_status(:conflict)
       end
