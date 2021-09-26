f284a9cd7072f3d0b96d593fb59d9ceeef1d68f7
Peichong <fupeichong@gmail.com>
2021-09-27 02:06:02 +0800

    UserRegistration spec - test form elements displayed correctly

--- spec/support/spec_helpers/general_spec_helpers.rb
+++ spec/support/spec_helpers/general_spec_helpers.rb
@@ -2,6 +2,11 @@ module GeneralSpecHelpers
   def login_user(credentials)
     post '/login', params: { session: credentials }
   end
+
+  def expect_field_with_label(field, label)
+    expect(page).to have_css("label[for=#{field}]", text: label)
+    expect(page).to have_field field
+  end
 end
 
 RSpec.configure do |config|
--- spec/system/user_registration_spec.rb
+++ spec/system/user_registration_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "UserRegistration", type: :system do
-  describe "page is displayed correctly" do
+  let(:form) { build :registration_form }
+
+  describe "page display" do
     before { visit '/register' }
 
     it "has correct title" do
       expect(page).to have_title('EZML | Registration', exact: true)
+    end
+
+    it "has correct form elements", js: true do
+      within '#user-form' do
+        expect(page).to have_css('.form-title', text: form[:title])
+        form[:fields].each {|field| expect_field_with_label *field.values}
+        expect(page).to have_button form[:submit]
+        expect(page).to have_link(form[:have_account], href: '/login')
+      end
     end
   end
 end
