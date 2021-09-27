1306ec7de398e63a5cb6d745453cb8a6a18e2c70
Peichong <fupeichong@gmail.com>
2021-09-27 08:14:32 +0800

    user registration spec - add test cases for valid input

--- spec/system/user_registration_spec.rb
+++ spec/system/user_registration_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "UserRegistration", type: :system do
-  let(:form) { build :registration_form }
+  let(:form)        { build :registration_form }
+  let(:valid_input) { attributes_for(:user).except(:role) }
 
   describe "page display" do
     before { visit '/register' }
@@ -17,6 +18,23 @@ RSpec.describe "UserRegistration", type: :system do
         expect(page).to have_button form[:submit]
         expect(page).to have_link(form[:have_account], href: '/login')
       end
+    end
+  end
+
+  context "when valid input", js: true do
+    before do
+      visit '/register'
+      valid_input.each {|key, val| fill_in "user_#{key}", with: val }
+    end
+
+    it "creates a new user account" do
+      expect { find('input[type="submit"]').click }.to change(User, :count).by 1
+    end
+
+    it "redirects to homepage after registration" do
+      expect {
+        find('input[type="submit"]').click
+      }.to change { current_path }.from('/register').to('/')
     end
   end
 end
