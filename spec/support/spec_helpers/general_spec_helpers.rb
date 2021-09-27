5abe55e5ee68885d805a31c6f093255e2ea8c204
Peichong <fupeichong@gmail.com>
2021-09-27 20:54:33 +0800

    spec helpers - add gui login method

--- spec/support/spec_helpers/general_spec_helpers.rb
+++ spec/support/spec_helpers/general_spec_helpers.rb
@@ -3,6 +3,12 @@ module GeneralSpecHelpers
     post '/login', params: { session: credentials }
   end
 
+  def gui_login_user(credentials, navigate_to_login: false)
+    visit '/login' if navigate_to_login
+    credentials.each {|key, val| fill_in "session_#{key}", with: val }
+    find('input[type="submit"]').click
+  end
+
   def expect_field_with_label(field, label)
     expect(page).to have_css("label[for=#{field}]", text: label)
     expect(page).to have_field field
