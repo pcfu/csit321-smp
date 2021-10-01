f6501f59d5552ef414e4fba47932afc29d8d1eb7
Lynn Ng <linglingling000@gmail.com>
2021-10-02 01:34:19 +0800

    modified portfolio frontend page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -26,4 +26,19 @@ class FavoritesController < ApplicationController
       format.js{}
     end
   end
+
+  def create
+    if Favorite.create(stock: Stock, user: current_user)
+      @favorite_exists = true
+    else
+      redirect_to @stock, alert: 'Something went wrong...*sad panda*'
+      @favorite_exists = false
+    end
+  end
+  
+  def destroy
+    Favorite.where(favorited_id: @project.id, user_id: current_user.id).first.destroy
+    redirect_to @project, notice: 'Project is no longer in favorites'
+  end
+
 end
--- app/javascript/packs/favorites/index.js
+++ app/javascript/packs/favorites/index.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     let table;
 
 
-    $('#fav-list tbody').on('click', 'tr', function () {
-    const stockId = $('#fav-list tr').data('fav').id;
-   // const stockId = $(this).data();
-    alert(stockId);
-    window.location.href = `/stocks/${stockId}`;
-    });
-  
+   
     $('#fav-list tbody tr').css('cursor', 'pointer');
+    $('td[data-link').css('color','blue');
+
+
+    $('td[data-link]').click(function(){
+        window.location = this.dataset.link
+    })
   
 });
