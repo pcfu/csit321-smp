cc8d0bf8a99438c81744f513a0491be440085127
Lynn Ng <linglingling000@gmail.com>
2021-10-15 08:57:33 +0800

    modified unfavorite and threshold button with the re-direction links

--- app/javascript/packs/favorites/index.js
+++ app/javascript/packs/favorites/index.js
@@ -1,31 +1,44 @@
 $(document).ready(function () {
     let table;
    
-    $('#fav-list tbody tr').css('cursor', 'pointer');
+    
     $(".fav_row").each(function(){
         var buy = ($(this).find($('.fav_buythreshold')).html());
         var sell = ($(this).find($('.fav_sellthreshold')).html());
         var close = ($(this).find($('.fav_close')).html());
         if (parseFloat(close)<parseFloat(buy)){
-            $(this).find($('.fav_buythreshold')).css('color','blue');
+            $(this).css('color','green');
             $(this).find($('.fav_buythreshold')).append(" [Buy Alert]");
-            $(this).find($('.fav_close')).css('color','blue');
     
         } else if (parseFloat(close)>parseFloat(sell)){
-            $(this).find($('.fav_sellthreshold')).css('color','red');
+            $(this).css('color','red');
             $(this).find($('.fav_sellthreshold')).append(" [Sell Alert]");
-            $(this).find($('.fav_close')).css('color','red');
         }
     });
     
 
    
       //  $('#fav-list tbody tr').setAttribute("class","table-danger");
+  //  $('.unclickable').on('click', function (e) {
+  //      e.stopPropagation(); 
+  //  })
 
-    $('#fav-list tbody tr').on('click', function () {
-        window.location = this.dataset.link
+    // pop up alert button
+/*     $('#unfav_button').on('click', function (e) {
+        var r = confirm("Confirm unfavoriting?");
+        if (r!=true){
+            e.preventDefault();
+        };
+        $('.popup').show(); 
+    })   */
+
+
+    //link to stock show page
+    $('#fav-list tbody tr').css('cursor', 'pointer');
+    $('#fav-list tbody tr').on('click', 'td:not(.unclickable)', function () {
+        var the_link = $(this).parent().attr("data-link");
+        window.location = the_link;
     })
-
 
 
   
