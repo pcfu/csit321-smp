2878ae51d5a6e7e7d18a235ca97a63c573f963d0
Peichong <fupeichong@gmail.com>
2021-10-30 00:40:40 +0800

    ModelTrainingsController - reset all trainings for Model on batch_enqueue

--- app/controllers/admin/model_trainings_controller.rb
+++ app/controllers/admin/model_trainings_controller.rb
@@ -8,8 +8,8 @@ module Admin
     def batch_enqueue
       with_error_handling do |flagger|
         config = ModelConfig.find(batch_enqueue_params[:config_id])
-        trngs = reset_trainings(config)
-        @response = enqueue_training_jobs(trngs, model_attributes(config))
+        config.reset_trainings([1, 5, 6])
+        @response = enqueue_training_jobs(training_data(config), model_data(config))
         flagger.flag(@response) if @response[:status] == 'error'
       end
     end
@@ -33,14 +33,13 @@ module Admin
               .permit(:stage, :rmse, :accuracy, :parameters, :error_message)
       end
 
-      def reset_trainings(model_config)
-        #model_config.reset_trainings(date_s, date_e)
+      def training_data(model_config)
         model_config.model_trainings.map do |t|
           { training_id: t.id, stock_id: t.stock_id, stock_symbol: t.stock.symbol }
         end
       end
 
-      def model_attributes(model_config)
+      def model_data(model_config)
         {
           model_name: model_config.name,
           model_class: model_config.model_type,
