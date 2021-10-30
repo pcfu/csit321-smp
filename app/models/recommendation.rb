e5603940b9b51e99f67998dfca1adf34dc7c62ba
Peichong <fupeichong@gmail.com>
2021-10-30 21:16:49 +0800

    Recommendation - model update

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,21 +1,7 @@
 class Recommendation < ApplicationRecord
-
-  ST_DAYS = 14
-  MT_DAYS = 90
-
   belongs_to :stock
-  before_validation :impute_dates
 
-  private
-    def impute_dates
-      if entry_date.present?
-        self.nd_date = entry_date.advance(days: 1) if nd_date.nil?
-        self.st_date = entry_date.advance(days: ST_DAYS) if st_date.nil?
-        self.mt_date = entry_date.advance(days: MT_DAYS) if mt_date.nil?
-      end
-    end
+  enum verdict: { buy: 'buy', hold: 'hold', sell: 'sell' }
 
-    # def broadcast_new_prediction
-    #   AdminChannel.broadcast EventMessages.price_prediction_creation(self)
-    # end
+  validates_presence_of :prediction_date, :verdict
 end
