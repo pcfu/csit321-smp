daf113783b5833348d0251eb50e2d269a8f63bff
Peichong <fupeichong@gmail.com>
2021-10-31 14:31:50 +0800

    stocks index page - only rank stocks that have price_histories

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,21 +1,15 @@
 class StocksController < ApplicationController
   def index
     @stocks = Stock.all
-
-    max_performer = 10
-    
-    latest_changes = Stock.all.map {|s|
+    latest_changes = @stocks.select {|s| s.price_histories.count > 0}.map {|s|
       change = s.price_histories.last.percent_change
       { symbol: s.symbol, change: change.to_f }
     }.sort_by {|s| s[:change]}.reverse
 
-    # latest_changes = [{:symbol=>"XOM", :change=>0.0258}, {:symbol=>"GSK", :change=>0.0003}, {:symbol=>"AAPL", :change=>0.0111}, {:symbol=>"WPM", :change=>-0.0078},
-    #   {:symbol=>"AAA", :change=>0.0258}, {:symbol=>"BBB", :change=>-0.0003}, {:symbol=>"CCC", :change=>0.0111}, {:symbol=>"DDD", :change=>0.2078},
-    #   {:symbol=>"EEE", :change=>0.0258}, {:symbol=>"FFF", :change=>0.2003}, {:symbol=>"GGG", :change=>0.8111}, {:symbol=>"HHH", :change=>0.1078}].sort_by {|s| s[:change]}.reverse
-
     best_changes = latest_changes.select{ |item| item[:change] > 0 }
     worst_changes = latest_changes.select{ |item| item[:change] <= 0 }
 
+    max_performer = 10
     count_best = best_changes.length() >= max_performer ? max_performer : best_changes.length()
     count_worst = worst_changes.length() >= max_performer ? max_performer : worst_changes.length()
 
