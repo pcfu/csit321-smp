20199d6758640e754a059c926caf01d136154ff9
Peichong <fupeichong@gmail.com>
2021-11-01 18:19:42 +0800

    SistersLookupTable - complete pairings

--- app/lib/sister_stocks_lookup_table.rb
+++ app/lib/sister_stocks_lookup_table.rb
@@ -1,8 +1,30 @@
 class SisterStocksLookupTable
   SISTERS = {
+    # Technology
     :AAPL => [ :IBM,  :AMZN ],
-    :IBM  => [ :AAPL, :AMZN ],
-    :AMZN => [ :AAPL, :IBM  ],
+    :IBM  => [ :AMZN, :CRM  ],
+    :AMZN => [ :CRM,  :INTC ],
+    :CRM  => [ :INTC, :CSCO ],
+    :INTC => [ :CSCO, :ORCL ],
+    :CSCO => [ :ORCL, :AAPL ],
+    :ORCL => [ :AAPL, :IBM  ],
+
+    # Energy
+    :XOM  => [ :BP,   :SNP  ],
+    :BP   => [ :SNP,  :TRP  ],
+    :SNP  => [ :TRP,  :COP  ],
+    :TRP  => [ :COP,  :DVN  ],
+    :COP  => [ :DVN,  :XOM  ],
+    :DVN  => [ :XOM,  :BP   ],
+
+    # Healthcare
+    :GSK  => [ :PFE,  :JNJ  ],
+    :PFE  => [ :JNJ,  :SNY  ],
+    :JNJ  => [ :SNY,  :MDT  ],
+    :SNY  => [ :MDT,  :BIO  ],
+    :MDT  => [ :BIO,  :ABT  ],
+    :BIO  => [ :ABT,  :GSK  ],
+    :ABT  => [ :GSK,  :PFE  ],
   }
   private_constant :SISTERS
 
