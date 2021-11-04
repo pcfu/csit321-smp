5d14f0e784b30a5621242537634ca3336dd747db
Peichong <fupeichong@gmail.com>
2021-11-04 15:32:40 +0800

    EventMessages - simplify model training progress event message

--- app/lib/event_messages.rb
+++ app/lib/event_messages.rb
@@ -1,7 +1,10 @@
 class EventMessages
   def self.model_config_training_progress(config)
-    ts = format_time(config.updated_at)
-    training_progress_message(config.name, config.train_percent, ts)
+    {
+      subject: "model training progress",
+      model_name: config.name,
+      percent: config.train_percent
+    }
   end
 
   def self.price_prediction_creation(prediction)
@@ -14,31 +17,11 @@ class EventMessages
 
   private
 
-    def self.training_progress_message(name, percent, timestamp)
-      msg = training_progress_base_message(percent, timestamp)
-      if percent == 100
-        msg[:body][:message] = "#{name} is 100% trained (#{Stock.count} stocks)"
-      end
-      msg
-    end
-
-    def self.training_progress_base_message(percent, timestamp)
-      {
-        subject: 'model_config',
-        context: percent == 100 ? 'success' : 'primary',
-        body: { train_percent: percent, updated_at: timestamp }
-      }
-    end
-
     def self.new_prediction_message(symbol, prices, message)
       {
         subject: 'price_prediction',
         context: 'success',
         body: { symbol: symbol, message: message }.merge(prices)
       }
-    end
-
-    def self.format_time(ts)
-      ts.strftime("%Y-%m-%d %H:%M:%S.%3N")
     end
 end
